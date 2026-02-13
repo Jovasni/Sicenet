8374d138b6d81e489ab449730e092bc7b5c9168b
JovaniZG <s22120168@alumnos.itsur.edu.mx>
2026-02-12 23:46:41 -0600

    Se creó el ViewModel del Login de la aplicación

---
+++ app/src/main/java/com/jovani/sicenet/ui/login/LoginViewModel.kt
@@ -0,0 +1,41 @@
+package com.jovani.sicenet.ui.login
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jovani.sicenet.data.repository.SicenetRepository
+import kotlinx.coroutines.launch
+
+class LoginViewModel : ViewModel() {
+    private val repository = SicenetRepository()
+
+    var matricula by mutableStateOf("")
+    var contrasenia by mutableStateOf("")
+
+    // Estados para el control de la UI
+    var isLoading by mutableStateOf(false)
+    var loginResult by mutableStateOf("")
+    var isSuccess by mutableStateOf(false)
+
+    fun onLoginClick() {
+        if (matricula.isBlank() || contrasenia.isBlank()) {
+            loginResult = "Por favor llena todos los campos"
+            return
+        }
+
+        viewModelScope.launch {
+            isLoading = true
+            val result = repository.login(matricula, contrasenia)
+
+            if (result != null && result.contains("accesoLoginResult")) {
+                loginResult = "¡Éxito! Cargando perfil..."
+                isSuccess = true
+            } else {
+                loginResult = "Error: Usuario o contraseña incorrectos"
+            }
+            isLoading = false
+        }
+    }
+}
